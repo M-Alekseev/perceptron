a4094f5e50bc353bb17d0bc7e0d209dbd9210f4b
Mikhail Alekseev <ironm1x@mail.ru>
2017-11-21 18:07:14 +0300

    perceptron update

--- perceptron.py
+++ perceptron.py
@@ -1,4 +1,4 @@
-from numpy import array, dot, random
+from numpy import array, dot, random, exp
 from random import choice
 
 
@@ -6,23 +6,23 @@ class Perceptron(object):
     def __init__(self):
         """
         - Initializes an array(1,3) of weights with random values from 0 to 1
-        - alpha is a learning rate
+        - eta is a learning rate
         - n the number of training steps
         - errors is a list to collect all errors
         """
         self.weights = random.rand(3)
-        self.alpha = 0.3
+        self.eta = 0.3
         self.n = 10000
         self.errors = []
 
     def step_function(self, x):
-        """
-        Step function implementation
-       """
         if x < 0:
             return 0
         else:
             return 1
+
+    def sigmoid(x):
+        return 1 / (1 + exp(-x))
 
     def train(self, train_data):
         for _ in range(self.n):
@@ -30,19 +30,27 @@ class Perceptron(object):
             result = dot(inputs, self.weights)
             error = expected - self.step_function(result)
             self.errors.append(error)
-            self.weights += self.alpha * error * inputs
+            self.weights += self.eta * error * inputs
 
-        for inputs, _ in train_data:
+    def test(self, test_data):
+        for inputs, _ in test_data:
             result = dot(inputs, self.weights)
             print("{}: {} -> {}".format(inputs[1:],
                                         result,
                                         self.step_function(result)))
 
 train_data = [
+    # i is input
+    # b is a bias
+    # O - desired output
+    #       b  i  i    O
     (array([1, 0, 0]), 0),
     (array([1, 0, 1]), 1),
     (array([1, 1, 0]), 1),
     (array([1, 1, 1]), 1),
 ]
-p = Perceptron()
-p.train(train_data)
+
+if __name__ == "__main__":
+    p = Perceptron()
+    p.train(train_data)
+    p.test(train_data)
