7075a7c7f1bcc61690eae0d5039f696163e479e7
Mikhail Alekseev <ironm1x@mail.ru>
2017-11-22 03:55:27 +0300

    modified for an image recognition

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# perceptron
-A simple python implementation of perceptron
+## Perceptron implementation for an image recognition 
+### About
+The perceptron can recognize selected patterns of 3x3 pixel black & white images.
+All images in this project are represent as 1D-arrays
+### Usage
--- perceptron.py
+++ perceptron.py
@@ -1,19 +1,24 @@
-from numpy import array, dot, random, exp
-from random import choice
+from numpy import array, dot, random  # , exp
 
 
 class Perceptron(object):
-    def __init__(self):
+    def __init__(self, *args):
         """
-        - Initializes an array(1,3) of weights with random values from 0 to 1
+        - Initializes an array(1,10) of weights with random values from 0 to 1
         - eta is a learning rate
         - n the number of training steps
-        - errors is a list to collect all errors
+        - errors is a list to collect all errors, u may use it for plotting
+        - selected contains number of inputs that we want to recognize
         """
-        self.weights = random.rand(3)
-        self.eta = 0.3
+        self.weights = random.rand(10)
+        self.eta = 0.003
         self.n = 10000
         self.errors = []
+        self.selected = random.randint(1, size=10)
+        # check if agrs fit the size of the weights
+        for arg in args:
+            assert (arg in range(len(self.weights))), "Out of range!"
+            self.selected[arg] = 1
 
     def step_function(self, x):
         if x < 0:
@@ -21,36 +26,65 @@ class Perceptron(object):
         else:
             return 1
 
-    def sigmoid(x):
-        return 1 / (1 + exp(-x))
+    # def sigmoid(self, x):
+    #     return 1 / (1 + exp(-x))
 
     def train(self, train_data):
-        for _ in range(self.n):
-            inputs, expected = choice(train_data)
-            result = dot(inputs, self.weights)
-            error = expected - self.step_function(result)
-            self.errors.append(error)
-            self.weights += self.eta * error * inputs
+        learned = False
+        iteration = 0
+        while not learned:
+            global_error = 0.0
+            for inputs, expected in zip(train_data, self.selected):
+                result = dot(inputs, self.weights)
+                error = expected - self.step_function(result)
+                self.weights += self.eta * error * inputs
+                global_error += abs(error)
+            iteration += 1
+            if global_error == 0 or iteration >= self.n:
+                learned = True
 
     def test(self, test_data):
-        for inputs, _ in test_data:
+        for inputs in test_data:
             result = dot(inputs, self.weights)
             print("{}: {} -> {}".format(inputs[1:],
                                         result,
                                         self.step_function(result)))
 
+"""
+    Each array in the list represents a 3x3 pixel image
+    where 1 is a white color and 0 accordingly black
+    | 1 | 0 | 0 |
+    | 0 | 1 | 0 |
+    | 0 | 0 | 1 |
+
+    First column is used for a bias value
+"""
 train_data = [
-    # i is input
-    # b is a bias
-    # O - desired output
-    #       b  i  i    O
-    (array([1, 0, 0]), 0),
-    (array([1, 0, 1]), 1),
-    (array([1, 1, 0]), 1),
-    (array([1, 1, 1]), 1),
+    #     |b |        |        |       |
+    # 1
+    array([1, 1, 1, 1, 0, 0, 0, 0, 0, 0]),
+    # 2   |b |        |        |       |
+    array([1, 0, 0, 0, 1, 1, 1, 0, 0, 0]),
+    # 3   |b |        |        |       |
+    array([1, 0, 0, 0, 0, 0, 0, 1, 1, 1]),
+    # 4   |b |        |        |       |
+    array([1, 1, 0, 0, 1, 0, 0, 1, 0, 0]),
+    # 5   |b |        |        |       |
+    array([1, 0, 1, 0, 0, 1, 0, 0, 1, 0]),
+    # 6   |b |        |        |       |
+    array([1, 0, 0, 1, 0, 0, 1, 0, 0, 1]),
+    # 7   |b |        |        |       |
+    array([1, 1, 0, 0, 0, 1, 0, 0, 0, 1]),
+    # 8   |b |        |        |       |
+    array([1, 0, 0, 1, 0, 1, 0, 1, 0, 0]),
+    # 9   |b |        |        |       |
+    array([1, 1, 0, 1, 0, 1, 0, 0, 0, 0]),
+    # 10  |b |        |        |       |
+    array([1, 0, 0, 0, 0, 1, 0, 1, 0, 1]),
 ]
 
 if __name__ == "__main__":
-    p = Perceptron()
+    # Class takes arguments in range [0-9]
+    p = Perceptron(2, 6)
     p.train(train_data)
     p.test(train_data)
